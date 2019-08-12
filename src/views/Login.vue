6895fc5b85aa7aefc67624798453854afd6e8fb9
Karson Peterson <d00321971@dmail.dixie.edu>
2019-08-12 17:36:06 -0600

    initial commit

---
+++ src/views/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="text" placeholder="Password"><br>
+        <button>Login</button>
+        <p>Don't have an account? Create one.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'login',
+    data() {
+        return {};
+    },
+    methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
